29d07d36316871606100ac0ce51851ecc7d7a25f
Yifeng Zhu <yifeng.zhu@utexas.edu>
2020-12-29 13:06:22 -0600

    Add a simple visualization of quaternion on SO3 sphere

--- robotics-tutorial/SO3_visualization.py
+++ robotics-tutorial/SO3_visualization.py
@@ -2,10 +2,12 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 
+from pyquaternion import Quaternion
 
-fig = plt.figure()
+
+fig = plt.figure(figsize=plt.figaspect(0.5)*1.5)
 ax = fig.add_subplot(111, projection='3d')
-ax.set_aspect('auto')
+
 # Make data
 u = np.linspace(0, 2 * np.pi, 100)
 v = np.linspace(0, np.pi, 100)
@@ -15,12 +17,31 @@ z = 1 * np.outer(np.ones(np.size(u)), np.cos(v))
 
 # Plot the surface
 ax.plot_surface(x, y, z, rstride=4, cstride=4, color='b', alpha=0.1)
+elev = 10.0
+rot = 80.0 / 180 * np.pi
+#calculate vectors for "vertical" circle
+a = np.array([-np.sin(elev / 180 * np.pi), 0, np.cos(elev / 180 * np.pi)])
+b = np.array([0, 1, 0])
+b = b * np.cos(rot) + np.cross(a, b) * np.sin(rot) + a * np.dot(a, b) * (1 - np.cos(rot))
 ax.plot(np.sin(u),np.cos(u),0,color='k', linestyle = 'dashed')
-ax.plot(np.sin(v),np.cos(v),0,color='k', linestyle = 'solid')
+horiz_front = np.linspace(0, np.pi, 100)
+ax.plot(np.sin(horiz_front),np.cos(horiz_front),0,color='k')
+vert_front = np.linspace(np.pi / 2, 3 * np.pi / 2, 100)
+ax.plot(a[0] * np.sin(u) + b[0] * np.cos(u), b[1] * np.cos(u), a[2] * np.sin(u) + b[2] * np.cos(u),color='k', linestyle = 'dashed')
+ax.plot(a[0] * np.sin(vert_front) + b[0] * np.cos(vert_front), b[1] * np.cos(vert_front), a[2] * np.sin(vert_front) + b[2] * np.cos(vert_front),color='k')
 
-ax.plot([0.0, 0.0, 0.0], [1.0, 0.0, 0.0], color='r')
+ax.view_init(elev = elev, azim = 0)
+
+q1 = Quaternion(w=0.5, x=0.5, y=-0.5, z=0.5)
+vec = q1.axis
+theta = q1.angle
+
+ax.plot([0.0, vec[0]], [0.0, vec[1]], [0.0, vec[2]], color='r')
+ax.scatter(vec[0], vec[1], vec[2])
+
 ax.grid(False)
 ax.set_xticks([])
 ax.set_yticks([])
 ax.set_zticks([])
+
 plt.show()
